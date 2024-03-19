27d235ee0351ee733d6ebab7ece2dffe011f7fe2
ANMOL_W_N <134546593+Anmol-ops-hash@users.noreply.github.com>
2024-03-19 17:50:35 +0000

    02_dataType

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // trat all JS code as newer version
+
+// alert( 3 + 3)// we are using nodejs, not browser
+
+console.log(3+
+    3);// code readability should be high
+
+console.log("Anmol")
+
+
+
+let  name = "anmol"
+let age = 18
+let isLogedIn= false
+
+
+// number => 2 to pwer 53
+// bigint
+//  string => ""
+//  boolean => true/false
+//  null=> standalone value
+//  undefined => 
+//  symbol => unique
+
+
+//  object
+
+console.log(typeof "anmol");
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
