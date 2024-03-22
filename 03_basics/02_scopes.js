6f325b3240585149b662a951fd33d54bd8ea4e31
ANMOL_W_N <134546593+Anmol-ops-hash@users.noreply.github.com>
2024-03-22 16:32:34 +0000

    function

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -7,10 +7,45 @@ if(true){
 
 }
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
     
+// }
+
+// console.log(a);
+// console.log(b);
+
+function one(){
+    const username = "anmol"
+
+    function two (){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+// one()
+if(true){
+    const username = "anmol"
+    if (username === "anmol"){
+        const website = " youtube"
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+// +++++++++++++++++++++++++++ interesting ++++++++++++++++++++++++++++++++
+
+console.log(addone(5))
+function addone(num){
+    return num + 1
 }
 
-console.log(a);
-// console.log(b);
+
+const addTwo = function(num){
+    return num + 2
+}
+
+addTwo(5)
